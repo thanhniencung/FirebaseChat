3989922bb18c3917ea9d2172ba8d6922142ff3d5
thanhniencung <ryan.vinova@gmail.com>
2017-01-15 01:04:35 +0700

    test

--- app/src/main/java/com/rubik/chatme/ui/activity/BaseActivity.java
+++ app/src/main/java/com/rubik/chatme/ui/activity/BaseActivity.java
@@ -28,6 +28,7 @@ public abstract class BaseActivity extends AppCompatActivity {
                 .build());
 
         setContentView(getLayout());
+        ButterKnife.bind(this);
     }
 
     protected AppComponent getAppComponent() {
